3c3f9735e0e1b7d96d84812dda622bf09bf9c87b
Ginna Alejandra <ginnavalenciaas@gmail.com>
2025-07-31 19:20:09 -0500

    Busqueda de peliculas

--- movie/views.py
+++ movie/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse # Clase usada para construir respuestas HTTP en django
 
+from .models import Movie
 # Create your views here.
 def home(request):
     #return HttpResponse('<h1>Welcome to Home Page</h1>')
     #return render(request, 'home.html') # Plantilla sin parámetros
-    return render(request, 'home.html', {'name':'Ginna Alejandra'})
+    #return render(request, 'home.html', {'name':'Ginna Alejandra'})
+    searchTerm = request.GET.get('searchMovie')
+    if searchTerm:
+        movies = Movie.objects.filter(title__icontains=searchTerms)
+    else:
+        movies = Movie.objects.all()
+    return render(request, 'home.html', {'searchTerms':searchTerm, 'movies':movies})
 
 def about(request):
     return render(request, 'about.html')
