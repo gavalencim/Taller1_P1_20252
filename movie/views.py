77ecf26162948236a4971c4a9ec1fd040902e3a3
Ginna Alejandra <ginnavalenciaas@gmail.com>
2025-08-21 21:33:05 -0500

    guardar cambios

--- movie/views.py
+++ movie/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse # Clase usada para construir respuestas HTTP en django
+import matplotlib.pyplot as plt
+import matplotlib
+import io
+import urllib, base64
 
 from .models import Movie
 # Create your views here.
@@ -16,4 +20,110 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')
+
+def statistics_views(request):
+    '''matplotlib.use('Agg')
+    years = Movie.objects.values_list('year', flat=True).distinct().order_by('year')
+    movie_counts_by_year = {}
+    for year in years:
+        if year:
+            movies_in_year = Movie.objects.filter(year=year)
+        else:
+            movies_in_year = Movie.objects.filter(year__isnull=True)
+            year = "None"
+        count = movies_in_year.count()
+        movie_counts_by_year[year] = count
+
+    bar_width = 0.5
+    bar_spacing = 0.5
+    bar_positions = range(len(movie_counts_by_year))
+
+    plt.bar(bar_positions, movie_counts_by_year.values(), width=bar_width, align='center')
+    # Personalizar la gráfica
+    plt.title('Movies per year')
+    plt.xlabel('Year')
+    plt.ylabel('Number of movies')
+    plt.xticks(bar_positions, movie_counts_by_year.keys(), rotation=90)
+    # Ajustar el espaciado entre las barras
+    plt.subplots_adjust(bottom=0.3)
+    # Guardar la gráfica en un objeto BytesIO
+    buffer = io.BytesIO()
+    plt.savefig(buffer, format='png')
+    buffer.seek(0)
+    plt.close()
+    # Convertir la gráfica a base64
+    image_png = buffer.getvalue()
+    buffer.close()
+    graphic = base64.b64encode(image_png)
+    graphic = graphic.decode('utf-8')
+    # Renderizar la plantilla statistics.html con la gráfica
+    return render(request, 'statistics.html', {'graphic': graphic})  '''
+    matplotlib.use('Agg')
+
+    # --- PRIMER GRÁFICO: PELÍCULAS POR AÑO ---
+    years = Movie.objects.values_list('year', flat=True).distinct().order_by('year')
+    movie_counts_by_year = {}
+
+    for year in years:
+        if year:
+            movies_in_year = Movie.objects.filter(year=year)
+        else:
+            movies_in_year = Movie.objects.filter(year__isnull=True)
+            year = "None"
+        count = movies_in_year.count()
+        movie_counts_by_year[year] = count
+
+    # Gráfico por año
+    bar_positions = range(len(movie_counts_by_year))
+    plt.bar(bar_positions, movie_counts_by_year.values(), width=0.5, align='center')
+    plt.title('Movies per year')
+    plt.xlabel('Year')
+    plt.ylabel('Number of movies')
+    plt.xticks(bar_positions, movie_counts_by_year.keys(), rotation=90)
+    plt.subplots_adjust(bottom=0.3)
+
+    buffer = io.BytesIO()
+    plt.savefig(buffer, format='png')
+    buffer.seek(0)
+    plt.close()
+    graphic_years = base64.b64encode(buffer.getvalue()).decode('utf-8')
+    buffer.close()
+
+    # --- SEGUNDO GRÁFICO: PELÍCULAS POR GÉNERO ---
+    
+    genres = Movie.objects.values_list('genre', flat=True).distinct().order_by('genre')
+    movie_counts_by_genre = {}
+
+    for genre in genres:
+        if genre:
+            genre1 = genre.split(',')[0].strip()
+        else:
+            movies_in_genre = Movie.objects.filter(genre__isnull=True)
+            genre1 = "None"
+        
+        #count = movies_in_genre.count()
+        #movie_counts_by_genre[genre1] = count
+        movie_counts_by_genre[genre1] = movie_counts_by_genre.get(genre1, 0) + 1
+
+    # Gráfico por género
+    bar_positions = range(len(movie_counts_by_genre))
+    plt.bar(bar_positions, movie_counts_by_genre.values(), width=0.5, align='center', color='orange')
+    plt.title('Movies per genre')
+    plt.xlabel('Genre')
+    plt.ylabel('Number of movies')
+    plt.xticks(bar_positions, movie_counts_by_genre.keys(), rotation=90)
+    plt.subplots_adjust(bottom=0.3)
+
+    buffer = io.BytesIO()
+    plt.savefig(buffer, format='png')
+    buffer.seek(0)
+    plt.close()
+    graphic_genres = base64.b64encode(buffer.getvalue()).decode('utf-8')
+    buffer.close()
+
+    # --- PASAR AMBOS GRÁFICOS AL TEMPLATE ---
+    return render(request, 'statistics.html', {
+        'graphic_years': graphic_years,
+        'graphic_genres': graphic_genres
+    })        
 
